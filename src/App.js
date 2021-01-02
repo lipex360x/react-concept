fefff9457f94638736f9c8368c404838aec16400
Felipe Borges <lipex360@gmail.com>
2021-01-02 18:56:59 -0300

    Props concept

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react'
 import Header from './components/Header'
 
 function App() {
-  return <Header />
+  return (
+    <Header title="Home Page">
+      <ul>
+        <li>HomePage</li>
+        <li>Projects</li>
+      </ul>
+    </Header>
+  )
 }
 
 export default App
